602478f3a18081ea11f69ccf8163ea3cf221a0b6
Harsh Pal <princeharshpal281515@gmail.com>
2024-11-10 22:34:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("I am root");
+});
+
+app.get(
+  "/testListing",
+  wrapAsync(async (req, res) => {
+    let sampleListing = new Listing({
+      title: "My new Villa",
+      description: "by the beach",
+      price: 1200,
+      location: "Calungute, Goa",
+      country: "India",
+    });
+    await sampleListing.save();
+    console.log("Successfull Testing");
+    res.send("successfull testing");
+  })
+);
+
+//index route
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    let allListings = await Listing.find({});
+    res.render("listings/index", { allListings });
+  })
+);
+
+//new route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new");
+});
+
+//show route
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/show", { listing });
+  })
+);
+
+//create route
+app.post(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Send valid data for listing");
+    }
+    const { title, description, image, location, country, price } = req.body;
+    const newListing = new Listing({
+      title,
+      description,
+      price,
+      location,
+      country,
+      image,
+    });
+
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+//edit route
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", { listing });
+  })
+);
+
+// update route
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Send valid data for listing");
+    }
+    const { id } = req.params;
+    const { title, description, image, price, location, country } = req.body;
+
+    await Listing.findByIdAndUpdate(id, {
+      title,
+      description,
+      image,
+      price,
+      location,
+      country,
+    });
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+//delete route
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not found"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something Went Wrong!" } = err;
+  res.status(statusCode).render("error", { err });
+});
+
+app.listen(3000, () => {
+  console.log("I'm listening");
+});
---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1549294413-26f195200c16?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG90ZWx8ZW58MHx8MHx8fDA%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1549294413-26f195200c16?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG90ZWx8ZW58MHx8MHx8fDA%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
