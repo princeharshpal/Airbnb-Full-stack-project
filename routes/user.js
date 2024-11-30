5d8fab1d1c6c83c9455d28fbe74dfa04ce13f73a
Harsh Pal <princeharshpal281515@gmail.com>
2024-11-30 22:44:23 +0530

    Authentication added (SignUp & LogIn)

---
+++ routes/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup");
+});
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      let newUser = new User({ email, username });
+      await User.register(newUser, password);
+      req.flash("success", "Welcome to wanderlust");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "welcome back to Wanderlust!")
+    res.redirect("/listings")
+  }
+);
+
+module.exports = router;
